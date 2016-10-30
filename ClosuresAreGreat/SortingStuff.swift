9f87ad299727425c59c9c1f9eb9976cd211d313f
jacqschweiger <jacqschweiger@gmail.com>
2016-10-30 19:25:34 -0400

    with print tests

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -15,7 +15,7 @@ struct ToyBin {
     var books: [Book] = []
     var bowlingPins: [BowlingPin] = []
     var musicCDs: [MusicCD] = []
-
+    
     
     mutating func sortShips() {
         ships = ships.sorted { (ship1: Ship, ship2: Ship) -> Bool in
@@ -38,86 +38,97 @@ struct ToyBin {
         books = books.sorted { return ($0.name < $1.name) }
         print(books)
     }
-
-
+    
+    
     mutating func sortBowlingPins() {
         bowlingPins = bowlingPins.sorted { return $0.color.rawValue < $1.color.rawValue }
         print(bowlingPins)
     }
     
+    
+    
+    
     mutating func sortMusicCDs() {
-        musicCDs = musicCDs.sorted { return $0.name > $1.name }
-        
-        for (index, cd) in musicCDs.enumerated() {
-            if cd.name == "Drake" {
-                musicCDs.remove(at: index)
-                musicCDs.insert(cd, at: 0)
+        musicCDs = musicCDs.sorted { (cd1: MusicCD, cd2: MusicCD) -> Bool in
+            
+            for (index, _) in musicCDs.enumerated() {
+                if cd1.name == "Drake" {
+                    musicCDs.remove(at: index)
+                    musicCDs.insert(cd1, at: 0)
+                    return true
+                } else if cd2.name == "Drake" {
+                    return false
+                }
             }
+            return cd1.name > cd2.name
         }
+        print(musicCDs)
+    }
+    
+        
+        mutating func changeColorOfAllPins(to color: Color) {
+            bowlingPins = bowlingPins.map { bowlingPin in
+                var newPin = bowlingPin
+                newPin.changeColor(to: color)
+                return newPin
+            }
+            print(bowlingPins)
+        }
+
+        
     }
     
     
-    mutating func changeColorOfAllPins(to color: Color) {
-        bowlingPins = bowlingPins.map { bowlingPin in
-            var newPin = bowlingPin
-            newPin.changeColor(to: color)
-            return newPin
-        }
-    }
-    
-}
-
-
 struct Ship: CustomStringConvertible {
-    var name: String
-    var age: Int
-    
-    var description: String {
-        return "Name: \(name), Age: \(age)"
+        var name: String
+        var age: Int
+        
+        var description: String {
+            return "Name: \(name), Age: \(age)"
+        }
     }
-}
-
-
-
+    
+    
+    
 struct Book: CustomStringConvertible {
-    var name: String
-    var year: Int
-    var author: String
-    
-    var description: String {
-        return "Name: \(name)"
+        var name: String
+        var year: Int
+        var author: String
+        
+        var description: String {
+            return "Name: \(name)"
+        }
     }
-}
-
-
+    
+    
 struct BowlingPin: CustomStringConvertible {
-    var name: String
-    var film: String
-    var color: Color
+        var name: String
+        var film: String
+        var color: Color
+        
+        var description: String {
+            return "Color: \(color)"
+        }
+    }
     
-    var description: String {
-        return "BowlingPin color: \(color)"
+    
+    extension BowlingPin {
+        mutating func changeColor(to color: Color) {
+            self.color = color
+        }
     }
-}
-
-
-extension BowlingPin {
-    mutating func changeColor(to color: Color) {
-        self.color = color
+    
+    
+    enum Color: Int {
+        case red, orange, yellow, green, blue, indigo, violet
     }
-}
-
-
-enum Color: Int {
-    case red, orange, yellow, green, blue, indigo, violet
-}
-
+    
 struct MusicCD: CustomStringConvertible {
-    var name: String
-    var year: Int
-    var songs: [String]
-    
-    var description: String {
-        return "CD name: \(name)"
-    }
+        var name: String
+        var year: Int
+        var songs: [String]
+        
+        var description: String {
+            return "Name: \(name)"
+        }
 }
--- ClosuresAreGreat/ViewController.swift
+++ ClosuresAreGreat/ViewController.swift
@@ -15,24 +15,49 @@ class ViewController: UIViewController {
         
         var bin = ToyBin()
 
-        let ship1 = Ship(name: "Titanic", age: 2)
-        let ship2 = Ship(name: "Big Ship", age: 5)
-        let ship3 = Ship(name: "Eleven", age: 9)
         
-        bin.ships = [ship1, ship2, ship3]
+        let ship1 = Ship(name: "Eleven", age: 9)
+        let ship2 = Ship(name: "Big Ship", age: 5)
+        let ship3 = Ship(name: "Titanic", age: 2)
+        let ship4 = Ship(name: "RowBoat", age: 7)
+        
+        bin.ships = [ship1, ship2, ship3, ship4]
         
         bin.sortShips()
         
         
+        let book1 = Book(name: "Gone With the Wine", year: 1929, author: "Margaret Mitchell")
+        let book2 = Book(name: "Pride and Prejudice", year: 1815, author: "Jane Austen")
+        let book3 = Book(name: "Alice In Wonderland", year: 1850, author: "Lewis Carroll")
+        
+        bin.books = [book1, book2, book3]
+        
+        bin.sortBooks()
         
         
         
+        let cd1 = MusicCD(name: "Reasonable Doubt", year: 1996, songs: ["Brooklyn's Finest", "Can't Knock the Hustle"])
+        let cd2 = MusicCD(name: "No Way Out", year: 1997, songs: ["Victory", "Been Around the World"])
+        let cd3 = MusicCD(name: "Drake", year: 2010, songs: ["Light Up", "Best I Ever Had"])
+        
+        bin.musicCDs = [cd1, cd2, cd3]
+        
+        bin.sortMusicCDs()
         
         
+        
+        let pin1 = BowlingPin(name: "PinC", film: "Harry Potter", color: .yellow)
+        let pin2 = BowlingPin(name: "PinA", film: "Bridget Jones", color: .orange)
+        let pin3 = BowlingPin(name: "PinB", film: "Star Wars", color: .red)
+        
+        bin.bowlingPins = [pin1, pin2, pin3]
+        
+        bin.sortBowlingPins()
         
         
         
     }
+    
     
   
     
